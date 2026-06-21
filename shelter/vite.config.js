03291ed4a28cb721f0a5f8bdfa89e40cb3a501fd
HoneySafonovets <kfs.boi@yandex.ru>
2026-06-21 21:28:26 +0300

    fix: vite.config.js

--- shelter/vite.config.js
+++ shelter/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, '/index.html'),
+        main: resolve(__dirname, 'index.html'),
         pets: resolve(__dirname, 'src/pets/pets.html'),
       },
       output: {

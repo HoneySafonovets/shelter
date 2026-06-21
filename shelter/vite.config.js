34eb4a43d26bc51a408f11bf04db526c978ece44
HoneySafonovets <kfs.boi@yandex.ru>
2026-06-21 19:14:10 +0300

    fix: structure of project

--- shelter/vite.config.js
+++ shelter/vite.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: './',
   build: {
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, '/index.html'),
+        pets: resolve(__dirname, 'src/pets/pets.html'),
+      },
       output: {
         assetFileNames: (assetInfo) => {
           if (assetInfo.name.endsWith('.css')) {

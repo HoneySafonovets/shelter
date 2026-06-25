61aaad9235bf57381289f87c5794e28e919d963f
HoneySafonovets <kfs.boi@yandex.ru>
2026-06-26 00:44:29 +0300

    feat: add pets.json and start code

--- shelter/src/main.js
+++ shelter/src/main.js
@@ -6,3 +6,12 @@ console.log(`
 • Layout matches the design at 768px +15
 • Layout matches the design at 320px +15
 `);
+let cardsArrayJSON = [];
+
+fetch('./pets.json')
+  .then(response => response.json())
+  .then(data => {
+    cardsArrayJSON = data;
+    console.log(cardsArrayJSON);
+});
+
